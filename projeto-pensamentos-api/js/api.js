4d63b2d24a2ea77c888be65bbda17230f1d21322
Gabriel Henrique Marino Denti <gabriel.denti@ogmoes.com.br>
2026-05-22 14:58:10 -0300

    curso de crud

--- projeto-pensamentos-api/js/api.js
+++ projeto-pensamentos-api/js/api.js
@@ -1,7 +1,9 @@
+const URL_BASE = 'http://localhost:3001';
+
 const api = {
     async buscarPensamentos(){
         try{
-            const resposta = await fetch('http://localhost:3001/pensamentos'); //Fetch por padrão usa get 
+            const resposta = await fetch(`${URL_BASE}/pensamentos`); //Fetch por padrão usa get
             return resposta.json();
         }catch(error){
             console.log(error.message);
@@ -9,7 +11,7 @@ const api = {
     },
      async salvarPensamento(pensamento){
         try{
-            const resposta = await fetch('http://localhost:3001/pensamentos', {
+            const resposta = await fetch(`${URL_BASE}/pensamentos`, {
                 method: 'POST', // Especifica o método HTTP como POST para indicar que estamos enviando dados para criar um novo recurso.
                 headers: {
                     "Content-Type": "application/json" // Define o cabeçalho Content-Type como application/json para informar ao servidor que o corpo da requisição contém dados no formato JSON.
@@ -23,7 +25,7 @@ const api = {
     },
     async buscarPensamentoPorId(id){
         try{
-            const resposta = await fetch(`http://localhost:3001/pensamentos/${id}`); 
+            const resposta = await fetch(`${URL_BASE}/pensamentos/${id}`); 
             return resposta.json();
         }catch(error){
             console.log(error.message);
@@ -31,12 +33,22 @@ const api = {
     },
     async editarPensamento(pensamento){
         try{
-            const resposta = await fetch(`http://localhost:3001/pensamentos/${pensamento.id}`, {
+            const resposta = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: 'PUT', // Especifica o método HTTP como PUT para indicar que estamos enviando dados para atualizar um recurso existente.
                 headers: {
                     "Content-Type": "application/json" // Define o cabeçalho Content-Type como application/json para informar ao servidor que o corpo da requisição contém dados no formato JSON.
                 },
                 body: JSON.stringify(pensamento) // Converte o objeto pensamento em uma string JSON para ser enviada no corpo da requisição.
+            });
+            return resposta.json();
+        }catch(error){
+            console.log(error.message);
+        }
+    },
+    async excluirPensamento(id){
+        try{
+            const resposta = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+                method: 'DELETE', // Especifica o método HTTP como DELETE para indicar que estamos enviando dados para excluir um recurso existente.
             });
             return resposta.json();
         }catch(error){
--- projeto-pensamentos-api/js/ui.js
+++ projeto-pensamentos-api/js/ui.js
@@ -3,16 +3,23 @@ import api from './api.js';
 const ui = {
     async renderizarPensamentos(){
         const listaPensamentos = document.getElementById('lista-pensamentos');
+        listaPensamentos.innerHTML = '';
 
         try{
             const pensamentos = await api.buscarPensamentos();
-            pensamentos.forEach(ui.adicionarPensamento);
+            if(pensamentos.length === 0){
+                ui.muralVazio(pensamentos);
+            }else{
+                pensamentos.forEach(ui.adicionarPensamento);
+            }
+
         }catch(error){
             console.error(error.message);
         }
     },
 
-    adicionarPensamento(pensamento){
+    adicionarPensamento(pensamento){  
+
         const listaPensamentos = document.getElementById('lista-pensamentos');
         const liPensamento = document.createElement('li');
         liPensamento.setAttribute('data-id', pensamento.id);
@@ -32,17 +39,34 @@ const ui = {
         pensamentoAutoria.textContent = pensamento.autoria;
 
         const btEditar = document.createElement('button');
-        pensamentoAutoria.classList.add('botao-editar');
+        btEditar.classList.add('botao-editar');
         btEditar.onclick = () => ui.preencherFormulario(pensamento);
 
         const iconeEditar = document.createElement('img');
         iconeEditar.src = 'assets/imagens/icone-editar.png';
         iconeEditar.alt = 'Ícone editar';
         btEditar.appendChild(iconeEditar);
-       
+
+        const btExcluir = document.createElement('button');
+        btExcluir.classList.add('botao-excluir');
+        btExcluir.onclick = async () => {
+            try{
+                await api.excluirPensamento(pensamento.id);
+                ui.renderizarPensamentos();
+            }catch(error){
+                console.error(error.message);
+            }
+        };
+
+        const iconeExcluir = document.createElement('img');
+        iconeExcluir.src = 'assets/imagens/icone-excluir.png';
+        iconeExcluir.alt = 'Ícone excluir';
+        btExcluir.appendChild(iconeExcluir);
+
         const icones = document.createElement('div');
         icones.classList.add('icones');
         icones.appendChild(btEditar);
+        icones.appendChild(btExcluir);
 
         liPensamento.appendChild(imgAspas);
         liPensamento.appendChild(pensamentoConteudo);
@@ -60,6 +84,32 @@ const ui = {
         document.getElementById('pensamento-autoria').value = pensameneto.autoria;
         document.getElementById('botao-salvar').textContent = 'Salvar alterações';
         
+    },
+
+    muralVazio(pensamentos){
+
+        const sectionMural = document.getElementById('lista-pensamentos-container');
+        const formulario = document.getElementById('form-container');
+
+        formulario.style.display = 'none';
+
+        const botaoAdicionar = document.createElement('button');
+        botaoAdicionar.textContent = 'Adicionar pensamentos';
+
+        
+        const p = document.createElement('p');
+        p.textContent = 'Nada por aqui ainda, que tal compartilhar alguma ideia?';
+        p.setAttribute('id', 'lista-pensamentos-container');
+
+        const img = document.createElement('img');
+        img.src = 'assets/imagens/lista-vazia.png';
+        img.alt = 'Lista vazia';
+        img.setAttribute('id', 'lista-pensamentos-container');
+
+        sectionMural.appendChild(p);
+        sectionMural.appendChild(img);
+        return sectionMural;
+
     }
 }
 
